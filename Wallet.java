602a72b9da2b9b8225cf62abcd2891757da5b7b7
VaranasiAsha <155607344+VaranasiAsha@users.noreply.github.com>
2024-03-19 17:58:38 +0530

    Update Wallet.java

--- Wallet.java
+++ Wallet.java
@@ -1,11 +1,27 @@
-//package com.sat.tmf.paymentscli.entity;
-
 public class Wallet {
-
-	//
-	//
-	//
-	private int userId;
+	
+	private int UserId;
+	private double balance;
+	private double walletAmountLimit =10000;
+	
+	public int getUserId() {
+		return UserId;
+	}
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getWalletAmountLimit() {
+		return walletAmountLimit;
+	}
+	public void setWalletAmountLimit(double walletAmountLimit) {
+		this.walletAmountLimit = walletAmountLimit;
+	}
+	
+	
 }
-
-
